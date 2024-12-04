2d3b3661b3fbd1410ae0e176fed79ef9e7769d14
Chris <chrisephotography@gmail.com>
2024-12-04 09:46:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,16 +15,16 @@ function populateFeed() {
                 <span class="username">${dataset[i].username}</span>
                 <span class="location">${dataset[i].location}</span>
               </div>
-              <button><span id="theme-toggle" class="material-icons-outlined">more_vert</span></button>
+              <button><span id="theme-toggle" class="material-icons">more_vert</span></button>
             </div>
             <img src="${dataset[i].photo}" />
             <div class="post-description">
               <p>${dataset[i].post}</p>
             </div>
             <div class="post-buttons">
-              <button><span id="theme-toggle" class="material-icons-outlined">favorite_border</span></button>
-              <button><span id="theme-toggle" class="material-icons-outlined">chat_bubble_outline</span></button>
-              <button><span id="theme-toggle" class="material-icons-outlined">send</span></button>
+              <button><span id="theme-toggle" class="material-icons">favorite</span></button>
+              <button><span id="theme-toggle" class="material-icons">chat_bubble</span></button>
+              <button><span id="theme-toggle" class="material-icons">send</span></button>
             </div>
           </div>
         </section>
