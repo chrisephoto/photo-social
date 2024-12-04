a78b19688cf666c39390d9d1969436d64f9ac72e
Chris <chrisephotography@gmail.com>
2024-12-04 08:51:40 -0500

    Create database.js

---
+++ database.js
@@ -0,0 +1,14 @@
+dataset = [
+  {
+    username: "@chrisephoto",
+    location: "Downtown Barbecue",
+    photo: "images/RDU6O6EAMBRF.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit nulla in tincidunt ullamcorper. Vestibulum nec sollicitudin libero, sit amet egestas dolor. Donec mattis varius pellentesque. Vestibulum sed dolor magna. This is a post which my contain things like <a href="#">@mentions</a> or <a href="#">#hastags</a>"
+  },
+  {
+    username: "@chrisephoto",
+    location: "Shenzhen, Guangdong, China",
+    photo: "images/NROW8INOVB8S.jpg",
+    description: "This is a post which my contain things like <a href="#">@mentions</a> or <a href="#">#hastags</a>"
+  }
+]
