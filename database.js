84e8db0eb9c35ba4cfffed6184e20cf3dbeac013
Chris <chrisephotography@gmail.com>
2024-12-04 09:27:59 -0500

    Update database.js

--- database.js
+++ database.js
@@ -4,13 +4,13 @@ dataset = [
     username: "@chrisephoto",
     location: "Downtown Barbecue",
     photo: "images/RDU6O6EAMBRF.jpg",
-    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit nulla in tincidunt ullamcorper. Vestibulum nec sollicitudin libero, sit amet egestas dolor. Donec mattis varius pellentesque. Vestibulum sed dolor magna. This is a post which my contain things like <a href="#">@mentions</a> or <a href="#">#hastags</a>"
+    post: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit nulla in tincidunt ullamcorper. Vestibulum nec sollicitudin libero, sit amet egestas dolor. Donec mattis varius pellentesque. Vestibulum sed dolor magna. This is a post which my contain things like <a href='#'>@mentions</a> or <a href='#'>#hastags</a>"
   },
   {
     avatar: "images/6NAU5RW9F7TQ.jpg",
     username: "@chrisephoto",
     location: "Shenzhen, Guangdong, China",
     photo: "images/NROW8INOVB8S.jpg",
-    post: "This is a post which my contain things like <a href="#">@mentions</a> or <a href="#">#hastags</a>"
+    post: "This is a post which my contain things like <a href='#'>@mentions</a> or <a href='#'>#hastags</a>"
   }
 ]
